a911880311e599cbc3715748c3c4c9046805f711
s-hellevikk <s-hellevikk@52S50009072.it.bsd405.org>
2019-01-07 18:00:05 -0800

    asDFXGHJBKNLM

--- src/org/usfirst/frc/team2412/robot/subsystems/IntakeInOutSubsystem.java
+++ src/org/usfirst/frc/team2412/robot/subsystems/IntakeInOutSubsystem.java
@@ -1,7 +1,10 @@
 package org.usfirst.frc.team2412.robot.subsystems;
 
+import org.usfirst.frc.team2412.robot.RobotMap;
+
 public class IntakeInOutSubsystem {
 intakeIn()
 intakeOut()
 intakeStop()
+private WPI_TalonSRX roller = RobotMap.rollerID
 }
